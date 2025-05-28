b2b845996ae6b62ec6e9d1a8f947a94e7a936958
ValdinarLopes <valdinarlopespessoaneto@gmail.com>
2025-05-27 21:10:58 -0300

    WIP: controller de autenticação adicionado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:120',
+            'email' => 'required|string|email|max:120|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['Credenciais inválidas.']
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
 }
